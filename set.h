3d758243cb66bdeb79078e3f0cc5ae748455ebf5
2084099671 <2084099671@qq.com>
2023-12-05 10:53:34 +0800

    set头文件

---
+++ set.h
@@ -0,0 +1,14 @@
+#ifndef SET_H_INCLUDED
+#define SET_H_INCLUDED
+#include<iostream>//c++库
+#include<algorithm>//算法库
+#include <stdio.h>//c标准库
+#include <stdlib.h>
+#include <string.h>//字符串
+using namespace std;//std命名空间
+
+
+#include "Cal_len_Huffman.h"
+#include "create_Huffman_tree.h"
+#include "print_Huffman_code.h"
+#endif // SET_H_INCLUDED
